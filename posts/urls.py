f6aabc4071df9e9020ad12479925ffc8d72703f7
Hongdoo Larry Jung <jja02059@gmail.com>
2020-04-07 12:56:53 +0900

    pass data from view to template

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from posts import views
+# from posts import views
+from . import views
 
 urlpatterns = [
-    path('', views.home)        
+    path('', views.home),
+    path('pass/', views.passView),
 ]
--- posts/views.py
+++ posts/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 
 def home(requests):
     return render(requests, "posts/home.html")
+
+
+def passView(requests):
+    data = {"message":"장고를 잘하자"}
+    return render(requests, "posts/pass.html", data)
